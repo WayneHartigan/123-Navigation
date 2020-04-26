5ae164318187772b3f0e122b7e384556d88c5727
Wayne Hartigan <hartiganwayne@gmail.com>
2020-04-26 21:13:45 +0100

    Make sure mic permission is given

--- permission.js
+++ permission.js
@@ -1,1 +1,14 @@
-navigator.mediaDevices.getUserMedia({audio: true})
+function getPermission(){
+  console.log("Got to permission functions");
+  navigator.mediaDevices.getUserMedia({audio: true})
+  .then((mediaStream) => {
+  //pass
+  console.log("pass");
+  })
+  .catch((error) => {
+    console.log(error)
+    getPermission();
+  });
+}
+
+getPermission();
