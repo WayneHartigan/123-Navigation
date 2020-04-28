74ebf0bd4f7689ec7355a57e516ba28b443262fd
Wayne Hartigan <hartiganwayne@gmail.com>
2020-04-28 17:09:44 +0100

    Recognition nevern ends and code organised much better

--- background/background.js
+++ background/background.js
@@ -1,108 +1,83 @@
-window.onload = getSpeech(null);
-function getSpeech (navValues){
+var navValues = null;
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var grammar = '#JSGF V1.0;'
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.continuous = false;
+recognition.start();
 
-  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-  var grammar = '#JSGF V1.0;'
-  var recognition = new SpeechRecognition();
-  var speechRecognitionList = new SpeechGrammarList();
-  speechRecognitionList.addFromString(grammar, 1);
-  recognition.grammars = speechRecognitionList;
-  recognition.lang = 'en-US';
-  recognition.interimResults = false;
-  recognition.continuous = true;
-
+recognition.onstart = function(event) {
+  console.log("Started")
+}
+recognition.onresult = function(event) {
+    speechResults(event, navValues);
+};
+recognition.onerror = function(event) {
+    console.log("Theres been an error trying to catch error!: " + event.error);
+}
+recognition.onend = function(event) {
+  console.log("Stopped");
   recognition.start();
+}
 
-  recognition.onstart = function(event) {
-    console.log("Started")
-  }
-
-  recognition.onresult = function(event) {
-      var last = event.results.length - 1;
-      var command = event.results[last][0].transcript;
-      console.log(command);
-      if (navValues){
-        if (command.toLowerCase().includes('cancel')){
-          sendMessagetoContext("cancelDom", null);
+function speechResults(event, navValues){
+  var last = event.results.length - 1;
+  var command = event.results[last][0].transcript;
+  if (navValues){
+    if (command.toLowerCase().includes('cancel')){
+      sendMessagetoContext("cancelDom", null);
+    }
+    else if (command.toLowerCase().includes('test')){
+      sendMessagetoContext("newDom", null);
+    }
+    else if (command.toLowerCase().includes('scroll down')){
+      sendMessagetoContext("scrollDown", null);
+    }
+    else if (command.toLowerCase().includes('scroll up')){
+      sendMessagetoContext("scrollUp", null);
+    }
+    else if (command.toLowerCase().includes('go back')){
+      sendMessagetoContext("goBack", null);
+    }
+    else if (command.toLowerCase().includes('go forward')){
+      sendMessagetoContext("goForward", null);
+    }
+    else {
+      var foundNav = false;
+      // iterate over each element in the array
+      searchTerm = command;
+      for (var i = 0; i < navValues.length; i++){
+        console.log(navValues[i].elementId);
+        if (command == navValues[i].navValue){
+          console.log(navValues[i]);
+          sendMessagetoContext("pressButton", navValues[i].elementId)
+          navValues = null;
+          foundNav = true;
+          break;
         }
-        else if (command.toLowerCase().includes('test')){
-          sendMessagetoContext("newDom", null);
-        }
-        else if (command.toLowerCase().includes('scroll down')){
-          sendMessagetoContext("scrollDown", null);
-        }
-        else if (command.toLowerCase().includes('scroll up')){
-          sendMessagetoContext("scrollUp", null);
-        }
-        else if (command.toLowerCase().includes('go back')){
-          sendMessagetoContext("goBack", null);
-        }
-        else if (command.toLowerCase().includes('go forward')){
-          sendMessagetoContext("goForward", null);
-        }
-        else {
-          var foundNav = false;
-          // iterate over each element in the array
-          searchTerm = command;
-          for (var i = 0; i < navValues.length; i++){
-            console.log(navValues[i].elementId);
-            if (command.includes(navValues[i].navValue)){
-              console.log(navValues[i]);
-              sendMessagetoContext("pressButton", navValues[i].elementId)
-              navValues = null;
-              foundNav = true;
-              break;
-            }
-          }
-          if (!foundNav){
-            getSpeech(navValues);
-          }
-        }
-      }
-      else{
-        if (command.toLowerCase().includes('test')){
-          sendMessagetoContext("newDom", null);
-        }
-        else if (command.toLowerCase().includes('scroll down')){
-          sendMessagetoContext("scrollDown", null);
-        }
-        else if (command.toLowerCase().includes('scroll up')){
-          sendMessagetoContext("scrollUp", null);
-        }
-        else if (command.toLowerCase().includes('go back')){
-          sendMessagetoContext("goBack", null);
-        }
-        else if (command.toLowerCase().includes('go forward')){
-          sendMessagetoContext("goForward", null);
-        }
-        else{
-          getSpeech(null);
-        }
-      }
-  };
-
-  var useless_error = false;
-  recognition.onerror = function(event) {
-    try{
-      if (event.error == "no-speech"){
-        useless_error = true;
-      }
-      else if (event.error == "aborted") {
-        //do nothing, not an error
-      }
-      else {
-        console.log("There has been an error: " + event.error);
       }
     }
-    catch (f){
-      console.log("Theres been an error trying to catch error!: " + f);
-    }
   }
-  recognition.onend = function(event) {
-    console.log("Stopped");
-    if (useless_error){
-      recognition.start();
+  else{
+    if (command.toLowerCase().includes('test')){
+      sendMessagetoContext("newDom", null);
+    }
+    else if (command.toLowerCase().includes('scroll down')){
+      sendMessagetoContext("scrollDown", null);
+    }
+    else if (command.toLowerCase().includes('scroll up')){
+      sendMessagetoContext("scrollUp", null);
+    }
+    else if (command.toLowerCase().includes('go back')){
+      sendMessagetoContext("goBack", null);
+    }
+    else if (command.toLowerCase().includes('go forward')){
+      sendMessagetoContext("goForward", null);
     }
   }
 }
@@ -112,19 +87,10 @@ function sendMessagetoContext (msg, objectToPress){
     chrome.tabs.sendMessage(tabs[0].id, {command: msg, objectToPress: objectToPress}, function(response){
       if (response.result.includes("Nav Icons")){
         var navList = response.navObjects;
-        getSpeech(navList);
+        navValues = navList;
       }
       else if (response.result.includes("Complete")){
-        getSpeech(null);
-      }
-      else{
-        try{
-          console.log(response.result);
-          getSpeech(null);
-        }
-        catch (e){
-          console.log(e);
-        }
+        navValues = null;
       }
     })
   })
---
+++ permission.js
@@ -0,0 +1,12 @@
+function getPermission(){
+  navigator.mediaDevices.getUserMedia({audio: true})
+  .then((mediaStream) => {
+    //pass
+  })
+  .catch((error) => {
+    console.log(error)
+    getPermission();
+  });
+}
+
+getPermission();
