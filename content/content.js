cd60d163f80d9308749cde482a4518e5a0e4c2bb
Wayne Hartigan <hartiganwayne@gmail.com>
2020-05-09 18:26:52 +0100

    Update functional format

--- content/content.js
+++ content/content.js
@@ -6,53 +6,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
         if (request.objectToPress){
             removeDom();
         }
-        //create new div to house navigating icons
-        var newDiv = document.createElement('div');
-        //give it some style
-        newDiv.setAttribute("id", "navigation-icons-div")
-        newDiv.setAttribute("style", "height:100%; width:100%; z-index:111111111111; position:fixed; top:0; left:0; text-align: center;");
-        //inject div into body of webpage
-        document.body.appendChild(newDiv);
-
-        //retireve and loop through each selected attribute
-        var pageAtts = document.querySelectorAll('*');
-
-        var navValueList = [];
-        var id = 1;
-        var navIconVal = 0;
-
-        for (att of pageAtts){
-            if (isElementInViewport(att) && checkIfClickable(att)){
-                //retrieving details of each selected attribute
-                var distance = att.getBoundingClientRect();
-                var newTop = distance.top;
-                var newLeft = distance.left;
-
-                // creating new span (navigating icons)
-                var navIcon = document.createElement('span');
-                //applying style
-                navIcon.setAttribute("class", "navIcon")
-                var style = "font-size: 16px; font-family: Arial, Helvetica, sans-serif; color: rgb(255, 255, 255); text-shadow: 0.6px 0.6px 0 #000; display: flex; align-items: center; justify-content: center; height: 22px; width: 22px; background-image: linear-gradient(to right, rgb(35, 131, 187) , rgb(112, 176, 197)); border: solid 1px black; box-shadow: 1px 0 4px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19); position: absolute; z-index:1111111111115656; top:"+newTop+"px; left:"+newLeft+"px;";
-                navIcon.setAttribute("style", style);
-                navIconVal++;
-                navValueList.push(navIconVal)
-
-                navIcon.innerHTML = navIconVal;
-
-                //injecting each span into created div
-                newDiv.appendChild(navIcon);
-                att.classList.add(navIconVal.toString());
-
-                var navObject = {
-                    "elementId" : att.className.toString(),
-                    "navValue": navIconVal
-                }
-                navObjectList.push(navObject);
-            }
-            else{
-                //pass
-            }
-        }
+        
+        createNavigationIcons(pageAtts);
         //send response to background.js allerting success
         if (navObjectList.length != 0){
             sendResponse({result: "Nav Icons", navObjects: navObjectList});
@@ -119,6 +74,57 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
         sendResponse({result: "Complete", navObjects: null});
     };
 });
+
+fucntion createBaseDiv() {
+    //create new div to house navigating icons
+    var newDiv = document.createElement('div');
+    //give it some style
+    newDiv.setAttribute("id", "navigation-icons-div")
+    newDiv.setAttribute("style", "height:100%; width:100%; z-index:111111111111; position:fixed; top:0; left:0; text-align: center;");
+    //inject div into body of webpage
+    document.body.appendChild(newDiv);
+}
+
+function createNavigationIcons(pageAtts) {
+    //retireve and loop through each selected attribute
+    var navValueList = [];
+    var id = 1;
+    var navIconVal = 0;
+
+    var pageAtts = document.querySelectorAll('*');
+    for (att of pageAtts){
+        if (isElementInViewport(att) && checkIfClickable(att)){
+            //retrieving details of each selected attribute
+            var distance = att.getBoundingClientRect();
+            var newTop = distance.top;
+            var newLeft = distance.left;
+
+            // creating new span (navigating icons)
+            var navIcon = document.createElement('span');
+            //applying style
+            navIcon.setAttribute("class", "navIcon")
+            var style = "font-size: 16px; font-family: Arial, Helvetica, sans-serif; color: rgb(255, 255, 255); text-shadow: 0.6px 0.6px 0 #000; display: flex; align-items: center; justify-content: center; height: 22px; width: 22px; background-image: linear-gradient(to right, rgb(35, 131, 187) , rgb(112, 176, 197)); border: solid 1px black; box-shadow: 1px 0 4px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19); position: absolute; z-index:1111111111115656; top:"+newTop+"px; left:"+newLeft+"px;";
+            navIcon.setAttribute("style", style);
+            navIconVal++;
+            navValueList.push(navIconVal)
+
+            navIcon.innerHTML = navIconVal;
+
+            //injecting each span into created div
+            newDiv.appendChild(navIcon);
+            att.classList.add(navIconVal.toString());
+
+            var navObject = {
+                "elementId" : att.className.toString(),
+                "navValue": navIconVal
+            }
+            navObjectList.push(navObject);
+        }
+        else{
+            //pass
+        }
+    }
+}
 
 function isElementInViewport(att) {
     var attDetails = att.getBoundingClientRect();
