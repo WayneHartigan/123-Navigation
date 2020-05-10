8259861ac7b77addaceb20d4f0d813497a268fc6
Wayne Hartigan <hartiganwayne@gmail.com>
2020-05-10 20:08:05 +0100

    Added comments

--- background/background.js
+++ background/background.js
@@ -1,3 +1,4 @@
+/* Setting up webkitSpeechRecognition */
 var navValues = null;
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
@@ -9,49 +10,79 @@ recognition.grammars = speechRecognitionList;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.continuous = false;
+/* End set up */
+
+// Starting recognition
 recognition.start();
 
+// When result is found
 recognition.onresult = function(event) {
     speechResults(event, navValues);
 };
+
+// When error is found
 recognition.onerror = function(event) {
+    // Check if error is "No-Speech" or "Not-Allowed"
+    // These are not real errors
     if (event.error != "no-speech" && event.error != "not-allowed"){
         console.log("Theres been an error: " + event.error);
     }
 };
+
+// When the recognition ends
 recognition.onend = function(event) {
+    // Start the recognition again
+    // This allows continuous interaction and recognition
     recognition.start();
 };
 
+
 function speechResults(event, navValues){
+    // Get the command from the webkitSpeechRecognition event. In lower case
     var last = event.results.length - 1;
     var command = event.results[last][0].transcript.toLowerCase();
+
+    // If navValues have been sent from context
     if (navValues){
+        // Check if the user has said a value in the list
         if (!checkForMatchingNavValue(command, navValues)){
+            // Else check if the users input matches the command list
             checkCommands(command);
         }
     }
+    // Else if the users input matches the command list
     else{
         checkCommands(command);
     }
 }
 
 function checkForMatchingNavValue(command, navValues) {
+    // Add a space each end of the users input
+    // This prevents the user saying "7" and "77" getting picked up
+    // "7" will become " 7 " & "77" will become " 77 "
     var commandCheck = " " + command + " ";
-    // iterate over each element in the array
+    // iterate over each element in the array to see if it matches any of the navicons values
     for (var i = 0; i < navValues.length; i++){
         var navCheck = " " + (navValues[i].navValue).toString() + " ";
         if (commandCheck.includes(navCheck)){
+            // If it does match, send that button id to the context class
             sendMessagetoContext("pressButton", navValues[i].elementId)
+            // set the nav values back to null
             navValues = null;
+            // return true so the command before knows not to look for any other command
             return true;
         }
     }
+    // if the user's input does not include a correct nav value id return false
+    // This will then check for other commands
     return false;
 }
 
 function checkCommands(command) {
+    // Checks for other commands such as scroll up, scroll down etc
     if (command.includes('navigation')){
+        // Sends a message to context with a specific message
+        // No button to click so pass in null
         sendMessagetoContext("newDom", null);
     }
     else if (command.includes('scroll down')){
@@ -79,22 +110,37 @@ function checkCommands(command) {
         sendMessagetoContext("refresh", null);
     }
     else if (command.includes('search for ')){
+        // if the user has said "search for {search term}"
+        // split the command afer "search for"
         var searchTerm = command.split('search for ').pop();
+        // send this message with the search term as the param
+        // Same param used to tell context which button to click
         sendMessagetoContext("search", searchTerm)
     }
 }
 
+/*
+    Sends a message to context
+    msg == command
+    objectToPress == whichever element the user has requested to click
+    objectToPress can also be used for the search param
+*/
 function sendMessagetoContext (msg, objectToPress){
     chrome.tabs.query({active: true, currentWindow:true}, function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, {command: msg, objectToPress: objectToPress}, function(response){
+            // If the response from context is undefined, do nothing, this prevents errors
             if (response === undefined){
                 //pass
             }
             else {
+                // If the response is "Nav Icons" it means the navigation icons have been created
+                // Set the navValues to the list of nav values returned from the context
                 if (response.result.includes("Nav Icons")){
                     var navList = response.navObjects;
                     navValues = navList;
                 }
+                // If its "completed" it means that some other command was completed
+                // This includes clicking the element so navValues is set back to null
                 else if (response.result.includes("Complete")){
                     navValues = null;
                 }
@@ -103,6 +149,7 @@ function sendMessagetoContext (msg, objectToPress){
     })
 };
 
+// When the extension is installed run the "welcome.html" page in a new tab
 chrome.runtime.onInstalled.addListener(function (object) {
     chrome.tabs.create({
         url: chrome.extension.getURL("html/welcome.html"),
--- content/content.js
+++ content/content.js
@@ -1,7 +1,14 @@
+// Listen for message send from background
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    // If request message is "new dom" it means the user has said "Navigation"
+    // This means we need to create the navigation icons
     if (request.command == 'newDom'){
+        // Lets create the nav icons
         navObjectList = createBaseDiv(request);
-        //send response to background.js allerting success
+
+        // Check the length of the navObjectList
+        // If it is empty, it means that no nav icons were created (probably no clickable elements on page)
+        // So no need to send a message back saying they've been created with an empty list
         if (navObjectList.length != 0){
             completeResponse("Nav Icons", navObjectList);
         }
@@ -9,126 +16,167 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             completeResponse("Complete", null);
         }
     }
+    // Else check if its another command
     else{
         checkOtherCommands(request);
         completeResponse("Complete", null);
     }
 
+    // if the user clicks or scrolls, the navigation icons should be removed
     document.onscroll = function(){
         removeDom();
         completeResponse("Complete", null);
-
     };
     document.onclick = function(){
         removeDom();
         completeResponse("Complete", null);
     };
 
+    // function to send the response back to the background
     function completeResponse(msg, navObjectList) {
         sendResponse({result: msg, navObjects: navObjectList});
     }
 });
 
+/*
+    This function is to create the base div that all navigation icons will be placed in
+    This ensures the navigation icons can be placed in the correct position for each element
+*/
 function createBaseDiv(request) {
-    //if command has been called twice, remove old dom.
-    if (request.objectToPress){
-        removeDom();
-    }
+    // Ensure there is not already nav icons on screen
+    // Remove old nav icons
+    removeDom();
+
     //create new div to house navigating icons
     var newDiv = document.createElement('div');
-    //give it some style
+
+    // Style the base div
+    // z-index is very high number to ensure it is always on top
     newDiv.setAttribute("id", "navigation-icons-div")
     newDiv.setAttribute("style", "height:100%; width:100%; z-index:111111111111; position:fixed; top:0; left:0; text-align: center;");
+
     //inject div into body of webpage
     document.body.appendChild(newDiv);
+
+    // return this function to allow the response to have a list of nav icons
     return createNavigationIcons(newDiv);
 }
 
+/* This function creates each div for all clickable elements on the webpage */
 function createNavigationIcons(newDiv) {
-    //retrieve and loop through each selected attribute
     var navObjectList = [];
-    var navValueList = [];
     var id = 1;
     var navIconVal = 0;
 
+    // Get list of all elements in web page
     var pageAtts = document.querySelectorAll('*');
+    // Loop through each element
     for (att of pageAtts){
+        // if the element is in the viewport and is clickable progress else just skip it
         if (isElementInViewport(att) && checkIfClickable(att)){
-            //retrieving details of each selected attribute
+            // get the pixel distance from top and left for the element
+            // this allows the nav icon to be in the correct position
             var distance = att.getBoundingClientRect();
             var newTop = distance.top;
             var newLeft = distance.left;
 
             // creating new span (navigating icons)
             var navIcon = document.createElement('span');
-            //applying style
+            // giving the nav icon a class of navIcon
             navIcon.setAttribute("class", "navIcon")
+            // give the icon some style with the top and left distance the same as the element it is assigned to
             var style = "font-size: 16px; font-family: Arial, Helvetica, sans-serif; color: rgb(255, 255, 255); text-shadow: 0.6px 0.6px 0 #000; display: flex; align-items: center; justify-content: center; height: 22px; width: 22px; background-image: linear-gradient(to right, rgb(35, 131, 187) , rgb(112, 176, 197)); border: solid 1px black; box-shadow: 1px 0 4px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19); position: absolute; z-index:1111111111115656; top:"+newTop+"px; left:"+newLeft+"px;";
             navIcon.setAttribute("style", style);
+            // bump up the number this allows infinite amounts of elemets to be used rather than a random number generator
             navIconVal++;
-            navValueList.push(navIconVal)
-
+            // setting the nav icon's html to the nav value
             navIcon.innerHTML = navIconVal;
 
-            //injecting each span into created div
+            //injecting each span into base div
             newDiv.appendChild(navIcon);
+
+            // adding the nav value to the class of the element
+            // this is used to click the element and keeps each element unique
             att.classList.add(navIconVal.toString());
 
+            // create the nav object to add to the list of objects to return
             var navObject = {
                 "elementId" : att.className.toString(),
                 "navValue": navIconVal
             }
             navObjectList.push(navObject);
         }
-        else{
-            //pass
-        }
     }
+    // return the list when the for loop is done
     return navObjectList;
 }
 
+/* This function checks the message for all other commands */
 function checkOtherCommands(request){
     if (request.command == 'pressButton' && request.objectToPress){
+        // get the object to press from the request
         elementId = request.objectToPress;
+        // click that element by searching by class name (all unique)
         document.getElementsByClassName(elementId.toString())[0].click();
+        // remove the dom (nav icons)
         removeDom();
 
     }
     else if (request.command == 'cancelDom'){
+        // remove the dom (nav icons)
         removeDom();
     }
     else if (request.command == 'scrollDown'){
+        // remove the dom (nav icons)
         removeDom();
+        // scroll up by 500 pixels
         window.scrollBy(0, 500);
     }
 
     else if (request.command == 'scrollUp'){
+        // remove the dom (nav icons)
         removeDom();
+        // scroll down by 500 pixels
         window.scrollBy(0, -500);
     }
     else if (request.command == 'goBack'){
+        // navigate to previous web page
         window.history.back();
     }
     else if (request.command == 'goForward'){
+        // navigate to next web page
         window.history.forward();
     }
     else if (request.command == 'goTop'){
+        // remove the dom (nav icons)
         removeDom();
+        // Scroll to the x,y of 0,0 (top of page)
         window.scrollTo(0, 0);
     }
     else if (request.command == 'goBottom'){
+        // remove the dom (nav icons)
         removeDom();
+        // Scroll to the x,y of 0,height of page (bottom of page)
         window.scrollTo(0,document.body.scrollHeight);
     }
     else if (request.command == 'search'){
+        // remove the dom (nav icons)
         removeDom();
+        // go to google search with the param of the search team
+        // "_self" is to open the search in the same tab
         window.open('http://google.com/search?q='+ request.objectToPress, "_self");
     }
     else if (request.command == 'refresh'){
+        // refresh the page
         location.reload();
     }
 }
 
+/*
+    This function checks if the element is in the current viewing window of the user
+    Prevents nav icons getting created for elements that the user cannot see
+    Returns true or false
+ */
 function isElementInViewport(att) {
     var attDetails = att.getBoundingClientRect();
         return (
@@ -139,6 +187,14 @@ function isElementInViewport(att) {
     );
 }
 
+/*
+    Checks if element is "clickable"
+        if element is not disabled it checks the following params
+        If element has an onclick or href it is clickable
+        if element is a button, textarea or input tag it is clickable
+        else it is not
+    Returns true or false
+ */
 function checkIfClickable(pageAtt){
     // Checks if element is disabled
     if (pageAtt.offsetWidth > 0 && pageAtt.offsetHeight > 0){
@@ -153,7 +209,12 @@ function checkIfClickable(pageAtt){
     return false;
 }
 
+/*
+    removes the base div created for the nav icons
+    also removes all elements in it (nav icons)
+*/
 function removeDom(){
+    // checks if the div is there before trying to delete, this prevents errors
     var dom = document.getElementById("navigation-icons-div")
     if (dom != null){
         dom.remove();
